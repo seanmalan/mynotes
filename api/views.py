cdd8a9ec2fe361e740c3469df75fb2c9eb347959
Sean Malan <smseanmalan@gmail.com>
2024-03-21 00:14:58 +1300

    adds serializer to the rest data

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
 # Create your views here.
 
 
@@ -44,3 +46,9 @@ def getRoutes(request):
   
   
   return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+  notes = Note.objects.all()
+  serializer = NoteSerializer(notes, many=True)
+  return Response(serializer.data)
